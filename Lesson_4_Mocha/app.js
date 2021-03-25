e04555c7a7e7b1ee3c7730802c308b8e630e8d46
Ernest Yedigaryan <Ernest_Yedigaryan@epam.com>
2021-03-26 01:11:02 +0400

    adding some new tests

--- Lesson_4_Mocha/app.js
+++ Lesson_4_Mocha/app.js
@@ -1,13 +1,30 @@
 class Calculator {
     add(...arr) {
+        if(arr.length == 0) {
+            return null;
+        }
+
         return arr.reduce((acc, cur) => acc + cur, 0);
+    }
+
+    minus(a,b) {
+        return a - b;
+    }
+
+    division(a,b) {
+        return a / b;
     }
     
     multiply(...arr) {
+        if(arr.length == 0) {
+            return null;
+        }
+        
         return arr.reduce((acc, cur) => acc * cur, 1);
     }
 }
 
 const calc = new Calculator;
+
 
 module.exports = calc;
--- Lesson_4_Mocha/test/appTest.js
+++ Lesson_4_Mocha/test/appTest.js
@@ -1,11 +1,14 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const C = require('../app'); 
+const axios = require('axios');
 
 describe('Calculations test', () => {
     const calc = C;
 
     it('check that calculation add returns number', () => {
-        assert.isNumber(calc.add(1,2,3,4,5));
+        const res = calc.add(1,2,3,4,5);
+        assert.isNumber(res);
     });
 
     it('check that calculation multiply returns number', () => {
@@ -16,7 +19,28 @@ describe('Calculations test', () => {
         assert.equal(15, calc.add(1,2,3,4,5));
     });
 
-    it('check that calculation nultiply(2,4,5) is equal to 40', () => {
+    it('check that calculation minus(41,20) is equal to 21', () => {
+        assert.equal(21, calc.minus(41,20));
+    });
+
+    it('check that calculation multiply(2,4,5) is equal to 40', () => {
         assert.equal(40, calc.multiply(2,4,5));
+    });
+
+    it('check that calculation division(27,3) is equal to 9', () => {
+        assert.equal(9, calc.division(27,3));
+    });
+
+    it('check add is not NaN', () => {
+        assert.isNotNaN(calc.add(27,3), 'is not NaN');
+    });
+
+    it('check if add has no args then returns  null', () => {
+        const res = calc.add();
+        expect(res).to.be.null;
+    });
+
+    it('check if multiply has no args then returns  null', () => {
+        expect(calc.multiply()).to.be.null;
     });
 });
